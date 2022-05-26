69e7688a408e291067177509509b15f22f9cf406
quangbao16t1 <bao16t1@gmail.com>
2022-05-26 15:56:03 +0700

    get count rate

--- server/src/repositories/rate.repository.js
+++ server/src/repositories/rate.repository.js
@@ -7,5 +7,6 @@ RateRepo.getRateById = (id) => RateService.getRateById(id);
 RateRepo.createRate = (rate) => RateService.createRate(rate);
 RateRepo.updateRate = (id, rate) => RateService.updateRate(id, rate);
 RateRepo.deleteRate = (id) => RateService.deleteRate(id);
+RateRepo.getRateByPostId = (postId) => RateService.getRateByPostId(postId);
 
 export default RateRepo;
