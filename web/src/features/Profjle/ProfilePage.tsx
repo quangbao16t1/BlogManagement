504aee35ad316cd3deeac0b093b886def4e40862
quangbao16t1 <bao16t1@gmail.com>
2022-06-03 09:27:58 +0700

    profile

--- web/src/features/Profjle/ProfilePage.tsx
+++ web/src/features/Profjle/ProfilePage.tsx
@@ -4,35 +4,35 @@ import girlImg from './girl.jpg';
 const ProfilePage = () => {
     return (
         <div className="clearfix">
-        <div className="row">
-            <div className="col-md-4 animated fadeIn" >
-              <div className="card">
-                <div className="card-body">
-                  <div className="avatar">
-                    <img
-                      src={girlImg}
-                      className="card-img-top"
-                      alt=""
-                    />
-                  </div>
-                  <h5 className="card-title">
-                    Nguyen quang Bao
-                  </h5>
-                  <p className="card-text">
-                   Quang nam
-                    <br />
-                    <span className="phone">12345333</span>
-                  </p>
+            <div className="row">
+                <div className="col-md-4 animated fadeIn" >
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="avatar">
+                                <img
+                                    src={girlImg}
+                                    className="card-img-top"
+                                    alt=""
+                                />
+                            </div>
+                            <h5 className="card-title">
+                                Nguyen quang Bao
+                            </h5>
+                            <p className="card-text">
+                                Quang nam
+                                <br />
+                                <span className="phone">12345333</span>
+                            </p>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
+            <button
+                className="btn btn-light btn-block w-50 mx-auto"
+            >
+                Load More Users
+            </button>
         </div>
-        <button
-          className="btn btn-light btn-block w-50 mx-auto"
-        >
-          Load More Users
-        </button>
-      </div>
     )
 }
 
